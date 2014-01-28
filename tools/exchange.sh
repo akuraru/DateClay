a2a97dc1c10a74cf26aa6b634070b80951218348
akuraru <akuraru@gmail.com>
2014-01-28 14:42:12 +0900

    いらないファイルを削除

--- tools/exchange.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/mogenerator -m "Project/Models/CoreData/Model.xcdatamodeld/Model.xcdatamodel" -O Project/Models/CoreData/Entity/ --template-var arc=true
-ruby tools/UserDefaultsCreator.rb Project/Models/UserDefaults/UserDefaults.h Project/Models/UserDefaults/ -f UserDefaults -i
-ruby tools/extractingId.rb Project/Storyboard/Main.storyboard Project/Storyboard/
